fdb95a6a52b7f9dc97a677633f60d4e1a102a7aa
Bartek Jachowicz <bartek.jachowicz@gmail.com>
2019-04-05 23:43:21 +0200

    Full inclusion-exclusion algo

--- inc-excAlgorithm.py
+++ inc-excAlgorithm.py
@@ -26,16 +26,20 @@ def create_adjacent_matrix(graph):
 def inc_exc_algorithm(graph):
     vertex_list = graph.vertex_list()
     sets = power_set(vertex_list)
+    result = 0
 
     for s in sets:
         x = np.setdiff1d(vertex_list, list(s))
         g_prim = graph.subgraph(x)
         h = Graph(g_prim)
-        create_adjacent_matrix(h)
-        # todo calculate n-th power of matrix
-        # todo get corresponding diagonal entry
-        pass
-    return 0
+        m = create_adjacent_matrix(h)
+        if len(m) >= 2:
+            matrix = np.linalg.matrix_power(m, len(vertex_list))
+            for k in range(len(m)):
+                result += (((-1) ** len(s)) * matrix[k][k])
+        elif len(m) == 1:
+            result += (((-1) ** len(s)) * (m[0][0]**len(vertex_list)))
+    return result
 
 
 def read_input():
